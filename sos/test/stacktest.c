99279a9d68f43596a94c1e0a1f29d1b314947b5e
Ratnaraj Mirgal <ratnaraj.mirgal@gmail.com>
2016-02-03 22:16:53 +0530

    stack test implementation

--- sos/test/stacktest.c
+++ sos/test/stacktest.c
@@ -1,71 +1,78 @@
+#include <stdio.h>
+
 #include "../rrm_config.h"
 #include "../os_config.h"
-//#include "../os_task.h"
 
 /*
    for checking task switching functionality
    task 1: blink led from 0 -> 3
    task 2: blink led from 4 -> 7
-   load stack
-   ldi r16, lo8(function)
-   out spl, r16
-   ldi r16, hi8(function)
-   out sph, r16
  */
 
 #define MY_STACK_SIZE	128
 
+typedef struct __task_control {
+	volatile uint8_t *stacktop;	//stack top
+	uint8_t *stack;		//stack starting
+	uint8_t ssize;		//stack size
+}task_control_t;
+
+static task_control_t task1;
+static task_control_t task2;
+
 static uint8_t t1stack[MY_STACK_SIZE];
 static uint8_t t2stack[MY_STACK_SIZE];
+
+task_control_t *current = NULL;	//current runnable task
+task_control_t *other = NULL;		//next runnable task (test code, not making use of find runnable function)
 
 typedef void (*taskf)(void *);
 
 //stackb bottom pointer, task function pointer
-static inline void __init_stack(uint8_t *stackb, taskf __task)
-{
-	uint16_t address = 0;
-
-	*stackb-- = 0xD8;
-
-	address = (uint16_t) __task;
-	*stackb-- = lo8(address);
-	*stackb-- = hi8(address);
-
-	*stackb-- = 0x00;	/*r0 */	
-	*stackb-- = 0x80;	/*SERG : sei*/	
-	*stackb-- = 0x00;	/*r1*/	
-	*stackb-- = 0x00;	/*r2*/	
-	*stackb-- = 0x00;	/*r3*/	
-	*stackb-- = 0x00;	/*r4*/	
-	*stackb-- = 0x00;	/*r5*/	
-	*stackb-- = 0x00;	/*r6*/	
-	*stackb-- = 0x00;	/*r7*/	
-	*stackb-- = 0x00;	/*r8*/	
-	*stackb-- = 0x00;	/*r9*/	
-	*stackb-- = 0x00;	/*r10*/	
-	*stackb-- = 0x00;	/*r11*/	
-	*stackb-- = 0x00;	/*r12*/	
-	*stackb-- = 0x00;	/*r13*/	
-	*stackb-- = 0x00;	/*r14*/	
-	*stackb-- = 0x00;	/*r15*/	
-	*stackb-- = 0x00;	/*r16*/	
-	*stackb-- = 0x00;	/*r17*/	
-	*stackb-- = 0x00;	/*r18*/	
-	*stackb-- = 0x00;	/*r19*/	
-	*stackb-- = 0x00;	/*r20*/	
-	*stackb-- = 0x00;	/*r21*/	
-	*stackb-- = 0x00;	/*r22*/	
-	*stackb-- = 0x00;	/*r23*/	
-
-	*stackb-- = 0x00;	/*r24*/	
-	*stackb-- = 0x00;	/*r25*/	
-
-	*stackb-- = 0x00;	/*r26*/	
-	*stackb-- = 0x00;	/*r27*/	
-	*stackb-- = 0x00;	/*r28*/	
-	*stackb-- = 0x00;	/*r29*/	
-	*stackb-- = 0x00;	/*r30*/	
-	*stackb-- = 0x00;	/*r31*/	
+#define __init_stack(stackb, __task) \
+{ \
+	uint16_t address = 0; \
+	*stackb-- = 0xD8; \
+	\
+	address = (uint16_t) __task; \
+	*stackb-- = lo8(address); \
+	*stackb-- = hi8(address); \
+	\
+	*stackb-- = 0x00;	/*r0 */	\
+	*stackb-- = 0x80;	/*SERG : sei*/	\
+	*stackb-- = 0x00;	/*r1*/	\
+	*stackb-- = 0x00;	/*r2*/	\
+	*stackb-- = 0x00;	/*r3*/	\
+	*stackb-- = 0x00;	/*r4*/	\
+	*stackb-- = 0x00;	/*r5*/	\
+	*stackb-- = 0x00;	/*r6*/	\
+	*stackb-- = 0x00;	/*r7*/	\
+	*stackb-- = 0x00;	/*r8*/	\
+	*stackb-- = 0x00;	/*r9*/	\
+	*stackb-- = 0x00;	/*r10*/	\
+	*stackb-- = 0x00;	/*r11*/	\
+	*stackb-- = 0x00;	/*r12*/	\
+	*stackb-- = 0x00;	/*r13*/	\
+	*stackb-- = 0x00;	/*r14*/	\
+	*stackb-- = 0x00;	/*r15*/	\
+	*stackb-- = 0x00;	/*r16*/	\
+	*stackb-- = 0x00;	/*r17*/	\
+	*stackb-- = 0x00;	/*r18*/	\
+	*stackb-- = 0x00;	/*r19*/	\
+	*stackb-- = 0x00;	/*r20*/	\
+	*stackb-- = 0x00;	/*r21*/	\
+	*stackb-- = 0x00;	/*r22*/	\
+	*stackb-- = 0x00;	/*r23*/	\
+	\
+	*stackb-- = 0x00;	/*r24*/	\
+	*stackb-- = 0x00;	/*r25*/	\
+	\
+	*stackb-- = 0x00;	/*r26*/	\
+	*stackb-- = 0x00;	/*r27*/	\
+	*stackb-- = 0x00;	/*r28*/	\
+	*stackb-- = 0x00;	/*r29*/	\
+	*stackb-- = 0x00;	/*r30*/	\
+	*stackb-- = 0x00;	/*r31*/	\
 }
 
 #define __save_all_regs() \
@@ -152,10 +159,26 @@ static inline void __init_stack(uint8_t *stackb, taskf __task)
 
 #define __save_sp() \
 { \
+	__asm__ volatile ( \
+		"lds	r26, current		\n\t" \
+		"lds	r27, current + 1	\n\t" \
+		"in	r0, __SP_L__		\n\t" \
+		"out	x+, r0			\n\t" \
+		"in	r0, __SP_H__		\n\t" \
+		"out	x+, r0			\n\t" \
+		::	); \
 }
 
 #define __load_sp() \
 { \
+	__asm__ volatile ( \
+		"lds	r26, current		\n\t" \
+		"lds	r27, current + 1	\n\t" \
+		"ld	r0, x+			\n\t" \
+		"out	__SP_L__, r0		\n\t" \
+		"ld	r0, x+			\n\t" \
+		"out	__SP_H__, r0		\n\t" \
+		::	); \
 }
 
 #define __save_ctx() \
@@ -170,34 +193,89 @@ static inline void __init_stack(uint8_t *stackb, taskf __task)
 	__load_all_regs(); \
 }
 
+static __start(void) __attribute__((naked));
+static __start(void)
+{
+	__load_ctx();
+	__asm__ volatile ("ret	\n\t" ::);	//should pop function address
+}
+
+#if 0
 static inline void __ctx_switch(void)
 {
+}
+#endif
+
+void __local_delay(void)
+{
+	uint16_t counter;
+	for(counter = 0; counter <= 0xfffe; counter++) {
+		__asm__ volatile(
+			"nop	\n\t"
+			"nop	\n\t"
+			"nop	\n\t"
+			"nop	\n\t"
+			::);
+	}
 }
 
 void blinker1(void *data)
 {
-	PORTC |= 0x00;
-	DDRC |= 0x0f;
+	uint8_t cnt = 0;
 	while (1) {
-
-		//switch context
+		cnt++;
+		PORTC = cnt;
+		__local_delay();
 	}
 }
 
 void blinker2(void *data)
 {
-	PORTC |= 0x00;
-	DDRC |= 0xf0;
+	uint8_t cnt = 0;
 	while (1) {
-
-		//switch context
+		cnt++;
+		PORTC = cnt;
+		__local_delay();
 	}
+}
+
+ISR(TIMER0_OVF_vect) //timer0 overflow
+{
+	task_control_t *tmp = current;
+	
+	__save_ctx();
+	current = other;
+	other = tmp;
+	__load_ctx();
 }
 
 int main(void)
 {
 	cli();
+	task1.stack = t1stack;
+	task1.stacktop = t1stack + (MY_STACK_SIZE - 1);
 
-	sei();
+	task2.stack = t2stack;
+	task2.stacktop = t2stack + (MY_STACK_SIZE - 1);
+
+	__init_stack((task1.stacktop), blinker1);
+	__init_stack((task2.stacktop), blinker2);
+
+	current = &task1;
+	other = &task2;
+
+	//init resources
+	PORTC = 0x00;
+	DDRC = 0xff;
+
+	//setup timer
+	AT_T0_OI_ENABLE;
+	AT_T0_PRESCALE64;
+	AT_T0_CNT_RESET;
+	
+	//sei(); //no need, load context will automatically set sei
+	__start();
+
+	//won't come here
 	return 0;
 }
